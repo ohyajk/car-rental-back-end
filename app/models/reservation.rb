b9a17fecc9c8042f7eede7ffb00afa6d87fe796e
EddyShimwa <shedpray16@gmail.com>
2023-05-15 14:04:07 +0200

    added the validations for reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,8 @@ class Reservation < ApplicationRecord
   belongs_to :car
   belongs_to :user
 
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :price_per_day, numericality: { greater_than_or_equal_to: 0 }
+  validates :city, presence: true, length: { maximum: 100 }
 end

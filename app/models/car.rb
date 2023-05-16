06fee2b32c3430c27fc3297b7981b775ac88a34b
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-05-16 11:49:58 +0200

    Add some validations to the Car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,9 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many :users, through: :reservations
+  validates :name, length: {greater_than: 3}
+  validates :description, length: {maximum: 2000}
+  validates :rent_per_day comparison: { greater_than_or_equal_to: 0 },
+  numericality: { only_integer: true }
+
 end

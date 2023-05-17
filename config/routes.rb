11fc0a21fb2c57eaa948f28cdc3e5ec14067804a
EddyShimwa <shedpray16@gmail.com>
2023-05-17 16:50:43 +0200

    setup the swagger and tools

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   resources :cars
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+ 
 end

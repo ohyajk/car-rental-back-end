8eaccdb67cf68510fcdc23c5f5bb5e09989540fd
EddyShimwa <shedpray16@gmail.com>
2023-05-19 21:06:43 +0200

    added the specs for reservation model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many :users, through: :reservations
-  
+
   validates :name, presence: true
   validates :description, presence: true, length: { maximum: 2000 }
   validates :rent_per_day, numericality: { greater_than_or_equal_to: 0 }
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    myuser = User.create(name: 'Eddy', username: 'pray', email: 'eddy@gmail.com', role: 'admin',
+                         password_digest: '123456')
+    mycar = Car.create(name: 'toyota', model: 'v8', description: 'the best car in the city', integer: 1, price: 200.7,
+                       rent_per_day: 2, user_id: myuser.id)
+    Reservation.create(
+      start_date: Date.today,
+      end_date: Date.tomorrow,
+      price_per_day: 50.00,
+      city: 'New York',
+      car_id: mycar.id,
+      user_id: myuser.id
+    )
+  end
+  before { subject.save }
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a price per one day' do
+      subject.price_per_day = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without a city' do
+      subject.city = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is should always have a city name' do
+      subject.city = 'Oklahoma'
+      expect(subject).to be_valid
+    end
+  end
+
+  describe 'associations' do
+    it 'belongs to a user' do
+      expect(subject.user).to be_instance_of(User)
+    end
+
+    it 'has many reservations' do
+      expect(subject.car).to be_instance_of(Car)
+    end
+  end
 end

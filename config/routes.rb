b7c3152f8074f3a1fa314a2dc7bb1c41422e9753
EddyShimwa <shedpray16@gmail.com>
2023-05-21 11:04:21 +0200

    added the tests for requests

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   post "/auth/login", to: "authentication#login"
-  
   resources :users
+  
   resources :reservations, only: [:index, :create, :destroy]
-  resources :cars, only: [:index]
+  resources :cars
   root to: redirect("/api-docs", status: 302)
 
 end

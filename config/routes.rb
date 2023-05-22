e1dcb8cf0a4dbe02a990c2f3515036aee8d34b19
EddyShimwa <shedpray16@gmail.com>
2023-05-22 15:33:11 +0200

    add api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  post "/auth/login", to: "authentication#login"
   
-  resources :users
-
   namespace :api do
     namespace :v1 do
       resources :reservations, only: [:index, :create, :destroy]
       resources :cars
+      resources :users
+      post '/signup', to: 'users#create'
+      post '/login', to: 'authentication#create'
     end
   end
   root to: redirect("/api-docs", status: 302)
 
-  post '/signup', to: 'users#create'
-  post '/login', to: 'authentication#create'
 end
